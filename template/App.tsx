73dad948be98e257f1ffa1354110a82abc770fd4
Lucas de Capra <lucas.capra@mobilesaude.com.br>
2024-01-11 14:41:28 -0300

    fix: add safe area view in the App Root

--- template/App.tsx
+++ template/App.tsx
@@ -1,22 +1,24 @@
 import React from 'react';
 
-import {ThemeProvider} from '@shopify/restyle';
-import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
+import { ThemeProvider } from '@shopify/restyle';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
 
-import {theme} from '@theme';
-import {ReactNativeReanimated} from "@components/ReactNativeReanimated";
+import { theme } from '@theme';
+import { ReactNativeReanimated } from '@components/ReactNativeReanimated';
 
 const queryClient = new QueryClient();
 
 export const App = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <SafeAreaProvider>
-        <ThemeProvider theme={theme}>
-          <ReactNativeReanimated/>
-        </ThemeProvider>
-      </SafeAreaProvider>
-    </QueryClientProvider>
-  );
+    return (
+        <QueryClientProvider client={queryClient}>
+            <SafeAreaProvider>
+                <ThemeProvider theme={theme}>
+                    <SafeAreaView>
+                        <ReactNativeReanimated />
+                    </SafeAreaView>
+                </ThemeProvider>
+            </SafeAreaProvider>
+        </QueryClientProvider>
+    );
 };
