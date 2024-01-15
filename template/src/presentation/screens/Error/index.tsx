13f5225188d19d58c25fa8e899ee891679ff5158
Lucas de Capra <lucas.capra@mobilesaude.com.br>
2024-01-15 17:26:25 -0300

    fix: fix error boundary screen

--- template/src/presentation/screens/Error/index.tsx
+++ template/src/presentation/screens/Error/index.tsx
@@ -4,29 +4,26 @@ import { StyleSheet, Text, TouchableOpacity } from 'react-native';
 import { FallbackProps } from 'react-error-boundary';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
-export const DefaultErrorScreen = ({
-  resetErrorBoundary,
-  error,
-}: FallbackProps) => {
-  return (
-    <SafeAreaView style={errorStyle.container}>
-      <Text>Error</Text>
-      <Text>Implement your default error here!</Text>
-      <Text style={errorStyle.errorMessage}>{error && error}</Text>
-      <TouchableOpacity onPress={() => resetErrorBoundary()}>
-        <Text>Reset</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
-  );
+export const DefaultErrorScreen = ({resetErrorBoundary, error,}: FallbackProps) => {
+    console.log('error:', error.message);
+    return (
+        <SafeAreaView style={errorStyle.container}>
+            <Text>Error</Text>
+            <Text>Implement your default error here!</Text>
+            <TouchableOpacity onPress={() => resetErrorBoundary()}>
+                <Text>Reset</Text>
+            </TouchableOpacity>
+        </SafeAreaView>
+    );
 };
 
 const errorStyle = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  errorMessage: {
-    fontWeight: 'bold',
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    errorMessage: {
+        fontWeight: 'bold',
+    },
 });
